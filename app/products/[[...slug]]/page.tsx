d388eabcec38c4bc20e738f9b31ac9484f0b9ad6
Tony Childs <tony.childs@gmail.com>
2023-09-19 13:53:38 -0500

    query params: product example

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>ProductPage: {slug?.join("/") ?? "Home"}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <>
+      <div>ProductPage: {slug?.join("/") ?? "Home"}</div>
+      <div>SortOrder param: "{sortOrder}"</div>
+    </>
+  );
 };
 
 export default ProductPage;
