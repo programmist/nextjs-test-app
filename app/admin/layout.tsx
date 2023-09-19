ae5fa996db43c736a8b3f1c99a01f2cd67328fef
Tony Childs <tony.childs@gmail.com>
2023-09-19 15:34:34 -0500

    Personal experiment: Create Admin settings page with admin sidebar nav

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { ReactNode } from "react";
 interface Props {
   children: ReactNode;
@@ -5,7 +6,14 @@ interface Props {
 const AdminLayout = ({ children }: Props) => {
   return (
     <div className="flex">
-      <aside className="bg-slate-200 p-5 mr-5">Admin Sidebar</aside>
+      <aside className="bg-slate-200 p-5 mr-5">
+        <div>
+          <Link href="/admin">Admin Home</Link>
+        </div>
+        <div>
+          <Link href="/admin/settings">Admin Settings</Link>
+        </div>
+      </aside>
       <div>{children}</div>
     </div>
   );
