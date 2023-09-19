98a915d046509e44d5a470c3bd1b4e1cbad8c495
Tony Childs <tony.childs@gmail.com>
2023-09-19 16:12:54 -0500

    Pass error object and retry function to error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,8 +1,21 @@
 "use client";
 import React from "react";
-
-const ErrorPage = () => {
-  return <div>An unexpected error has occurred</div>;
+interface Props {
+  error: Error;
+  reset: () => void;
+}
+const ErrorPage = ({ error, reset }: Props) => {
+  // Use logging service like sentry
+  console.log(error.message);
+  return (
+    <>
+      <div>An unexpected error has occurred:</div>
+      {/* Use this sparingly or our logs will suffer  */}
+      <button onClick={() => reset()} className="btn">
+        Retry?
+      </button>
+    </>
+  );
 };
 
 export default ErrorPage;
