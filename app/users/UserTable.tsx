80c2d402bd88e88588a9cd2e796ffa64a7d62520
Tony Childs <tony.childs@gmail.com>
2023-09-19 13:29:53 -0500

    routing overview: extract <UserTable />

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UserTable = async () => {
+  let res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((u) => (
+          <tr key={u.id}>
+            <td>{u.name}</td>
+            <td>{u.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,38 +1,14 @@
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-const UsersPage = async () => {
+const UsersPage = () => {
   // Note caching only happens in fetch function
   // revalidate cache every 10 seconds
-  let res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
-  });
-  const users: User[] = await res.json();
 
   return (
     <>
       <h1 className="font-bold">Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((u) => (
-            <tr key={u.id}>
-              <td>{u.name}</td>
-              <td>{u.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
