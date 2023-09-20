ecb0e180ff4b9658327c84694757890164b0400d
Tony Childs <tony.childs@gmail.com>
2023-09-20 14:31:37 -0500

    Added upload widget options from demo.cloudinary.com/uw/#/

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -21,6 +21,35 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="jy52onqx"
+        options={{
+          sources: ["local"],
+          multiple: true,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#ffffff",
+              sourceBg: "#f4f4f5",
+              windowBorder: "#90a0b3",
+              tabIcon: "#000000",
+              inactiveTabIcon: "#555a5f",
+              menuIcons: "#555a5f",
+              link: "#0433ff",
+              action: "#339933",
+              inProgress: "#0433ff",
+              complete: "#339933",
+              error: "#cc0000",
+              textDark: "#000000",
+              textLight: "#fcfffd",
+            },
+            fonts: {
+              default: null,
+              "'Space Mono', monospace": {
+                url: "https://fonts.googleapis.com/css?family=Space+Mono",
+                active: true,
+              },
+            },
+          },
+        }}
         onUpload={(result, widget) => {
           if (result.event !== "success") return;
           const info = result.info as CloudinaryResult;
