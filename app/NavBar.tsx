1ceaee75f582d54fd531ba783d06ccac76a0f2ab
Tony Childs <tony.childs@gmail.com>
2023-09-20 16:23:21 -0500

    Add AuthProvider and get auth info using client component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import Spinner from "./components/Spinner";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
+  const isAuth = status === "authenticated";
+
   return (
     <div className="flex space-x-3 p-5 bg-slate-300">
       <Link className="mr-5" href="/">
@@ -9,7 +16,11 @@ const NavBar = () => {
       </Link>
       <Link href="/admin">Admin</Link>
       <Link href="/users">Users</Link>
-      <Link href="/api/auth/signin">Log in</Link>
+      {status === "loading" && <Spinner />}
+      {status === "authenticated" && <div>{session.user?.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Log in</Link>
+      )}
     </div>
   );
 };
