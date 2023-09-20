7b9eb2ea731ad9fe93a6f78a4ca565b3029e3a62
Tony Childs <tony.childs@gmail.com>
2023-09-20 14:25:38 -0500

    Use onUpload event to store image ID and display after upload

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,39 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="jy52onqx">
-      {({ open }) => (
-        <button onClick={() => open()} className="btn btn-primary">
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="uploaded image"
+        />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="jy52onqx"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button onClick={() => open()} className="btn btn-primary">
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
