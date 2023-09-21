65ed7c9b6c33a099297a0bd2496e5bf3ea502d3f
Tony Childs <tony.childs@gmail.com>
2023-09-21 09:28:45 -0500

    Move auth options to spearate file; add GithubProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,5 @@
-import prisma from "@/prisma/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "Email" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials, req) {
-        // Add logic :here to look up the user from the credentials supplied
-        if (!credentials?.email || !credentials.password) return null;
-
-        // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-
-        // Any object returned will be saved in `user` property of the JWT
-        if (!user) return null;
-
-        const passwordsMatch = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword!
-        );
-
-        return passwordsMatch ? user : null;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "../authOptions";
 
 const handler = NextAuth(authOptions);
 
---
+++ app/api/auth/authOptions.ts
@@ -0,0 +1,55 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials, req) {
+        // Add logic :here to look up the user from the credentials supplied
+        if (!credentials?.email || !credentials.password) return null;
+
+        // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // Any object returned will be saved in `user` property of the JWT
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+
+        return passwordsMatch ? user : null;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
