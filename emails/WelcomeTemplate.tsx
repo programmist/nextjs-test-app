4e5ced84707a5e8def0f78bcbcefc625b7cb42bc
Tony Childs <tony.childs@gmail.com>
2023-09-21 18:11:55 -0500

    styling email template with inline styles (don't)

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,20 +6,28 @@ import {
   Link,
   Preview,
 } from "@react-email/components";
-import React from "react";
+import React, { CSSProperties } from "react";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome Aboard</Preview>
-      <Body>
+      <Body style={body}>
         <Container>
-          <Text>Hello {name}</Text>
+          <Text style={heading}>Hello {name}</Text>
           <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
         </Container>
       </Body>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
