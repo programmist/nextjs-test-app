9899f0cbde304b44b1c88ab0e748d9d5562f491b
Tony Childs <tony.childs@gmail.com>
2023-09-21 18:13:30 -0500

    styling email templates with <Tailwind> provider

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -2,6 +2,7 @@ import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,12 +13,14 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome Aboard</Preview>
-      <Body style={body}>
-        <Container>
-          <Text style={heading}>Hello {name}</Text>
-          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
