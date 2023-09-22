fda7d51dcb85b64323c77a19d52a17cbdb03d2c7
Tony Childs <tony.childs@gmail.com>
2023-09-21 22:54:54 -0500

    Messing with fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,25 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto, Roboto_Mono } from "next/font/google";
+import localFont from "next/font/local";
+import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
 import "./globals.css";
-import Script from "next/script";
-import GoogleAnalyticsScript from "./GoogleAnalyticsScript";
 
 const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"], // not necessary for "variable font"
+});
+export const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  weight: ["400", "500"], // not necessary for "variable font"
+});
+
+export const poppins = localFont({
+  src: "../public/fonts/poppins-regular-webfont.woff2",
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <GoogleAnalyticsScript />
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <AuthProvider>
           <NavBar />
           <main className="p-5">{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,30 @@ import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/authOptions";
 import coffee from "../public/coffee.jpg";
+import { poppins, robotoMono } from "./layout";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  console.log(poppins.variable);
+  console.log(poppins.className);
 
   return (
     <main className="relative h-screen">
       {/* local image */}
       <Image src={coffee} alt="Coffee" width={600} height={300} />
-
+      <h1>Hello World Default (Should be Poppins, but nope)</h1>
+      <h1 className={poppins.className}>Hello World Poppins.className</h1>
+      <h1 className={poppins.variable}>Hello World Poppins.variable</h1>
+      <h1 className="font-poppins">Hello World Poppins (not working)</h1>
+      <h1 className={robotoMono.className}>Hello World Roboto Mono</h1>
       {/* Remote image */}
-      <Image
+      {/* <Image
         src="https://bit.ly/react-cover"
         alt="Coffee"
         fill
         className="object-cover"
         priority
-      />
+      /> */}
     </main>
   );
 }
